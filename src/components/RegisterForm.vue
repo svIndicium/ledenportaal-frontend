20f0609ef189125967307761fb98968973bc88e1
Siard <siard.ijpma@indicium.hu>
2024-08-04 18:28:33 +0200

    Add expandable info panel to RegisterForm.vue

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -5,7 +5,13 @@
       label="E-mailadres"
       outlined
       dense
-    > <template #details> Gebruik het e-mailadres dat je hebt opgegeven bij je inschrijving. </template>
+    >
+
+      <template #details>
+        <span class="cursor-pointer" @mousedown="openPanel">
+          Zie <span class="text-decoration-underline"> informatie </span>
+        </span>
+      </template>
     </v-text-field>
     <v-text-field
       v-model="password"
@@ -14,6 +20,29 @@
       dense
       type="password"
     />
+
+    <v-expansion-panels variant="accordion" :model-value="informationPanel">
+      <v-expansion-panel
+        title="Informatie"
+      >
+        <v-expansion-panel-text>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title>E-mailadres</v-list-item-title>
+              <v-list-item-subtitle>Gebruik het e-mailadres dat je hebt opgegeven bij je inschrijving</v-list-item-subtitle>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-title>Wachtwoord</v-list-item-title>
+              <v-list-item-subtitle>Gebruik altijd een sterk en veilig wachtwoord.</v-list-item-subtitle>
+              <v-list-item-subtitle>Indicium heeft geen toegang tot wachtwoorden.</v-list-item-subtitle>
+              <v-list-item-subtitle>Authenticatie gaat via Google met Firebase.</v-list-item-subtitle>
+            </v-list-item>
+          </v-list>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
     <v-btn type="submit" color="secondary" block class="mt-4">
       Registreren
     </v-btn>
@@ -28,6 +57,10 @@ import { useFirebaseAuth } from 'vuefire'
 const auth = useFirebaseAuth();
 const username = ref('');
 const password = ref('');
+
+let informationPanel = ref(null);
+
+const openPanel = () => informationPanel.value = [0];
 
 const emit = defineEmits(['register-success']);
 

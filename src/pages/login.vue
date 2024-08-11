3278b4ef4ebd3134304f5fccaa18cda771783f9e
Siard <siard.ijpma@indicium.hu>
2024-08-11 22:00:19 +0200

    Responsify login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,39 +1,40 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-center fill-height mx-auto" max-width="500">
-      <div class="text-center">
-        <div class="text-body-2 font-weight-light mb-n1">
-          Welcome to Indicium
-        </div>
-        <h1 class="text-h2 font-weight-bold">Ledenportaal</h1>
-      </div>
+  <v-container class="fill-height pa-2 pa-sm-3">
+    <v-row justify="center" align="center" no-gutters>
+      <v-col cols="12" style="max-width: 500px;">
+        <v-responsive class="align-center text-center">
+          <div class="text-body-2 font-weight-light mb-n1">
+            Welkom bij Indicium
+          </div>
+          <h1 class="text-h3 text-sm-h3 text-md-h2 font-weight-bold">Ledenportaal</h1>
 
-      <div class="py-6" />
+          <div class="py-4" />
 
-      <v-tabs v-model="tab" centered>
-        <v-tab value="login">Login</v-tab>
-        <v-tab value="register">Register</v-tab>
-      </v-tabs>
+          <v-tabs v-model="tab" centered>
+            <v-tab value="login">Login</v-tab>
+            <v-tab value="register">Register</v-tab>
+          </v-tabs>
 
-      <v-window v-model="tab">
-        <v-window-item value="login">
-          <LoginForm @login-success="onLoginSuccess" />
-        </v-window-item>
-        <v-window-item value="register">
-          <RegisterForm @register-success="onRegisterSuccess" />
-        </v-window-item>
-      </v-window>
+          <v-window v-model="tab">
+            <v-window-item value="login">
+              <LoginForm @login-success="onLoginSuccess" />
+            </v-window-item>
+            <v-window-item value="register">
+              <RegisterForm @register-success="onRegisterSuccess" />
+            </v-window-item>
+          </v-window>
 
-      <v-row justify="center" class="mt-4">
-        <v-col cols="auto">
-          <v-btn @click="ping"> {{ result }} </v-btn>
-        </v-col>
-        <v-col cols="auto">
-<!--          TODO: Implement-->
-          <v-btn> Wachtwoord vergeten </v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
+          <v-row justify="center" class="mt-4">
+            <v-col cols="auto">
+              <v-btn block> Wachtwoord vergeten </v-btn>
+            </v-col>
+            <v-col cols="auto">
+              <v-btn @click="ping" block> {{ result }} </v-btn>
+            </v-col>
+          </v-row>
+        </v-responsive>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
